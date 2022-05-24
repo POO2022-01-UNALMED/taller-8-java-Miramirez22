6b73962312d28a66c1bac3566d650221cb167445
Miramirez22 <miramirez@unal.edu.co>
2022-05-24 11:13:12 -0500

    Create Portero.java

---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,24 @@
+package futbol;
+
+public class Portero extends Futbolista {
+	public short golesRecibidos;
+	public byte dorsal;
+	
+	public Portero(String nombre, int edad, short goles, byte dorsal) {
+		super(nombre,edad,"Portero");
+		this.golesRecibidos=goles;
+		this.dorsal=dorsal;
+	}
+
+	public int compareTo(Object p) {
+		return this.golesRecibidos-((Portero)p).golesRecibidos;
+	}
+
+	public boolean jugarConLasManos() {
+		return true;
+	}
+	public String toString() {
+		return "El futbolista "+getNombre()+" tiene: "+getEdad()+" y juega de: "+getPosicion()+" Con el dorsal: "+dorsal+" Le han marcado: "+golesRecibidos;
+	}
+
+}
